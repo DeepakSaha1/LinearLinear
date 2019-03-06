4e5e7d37521887bd4992a4632f13073d2bde1b57
DeepakSaha1 <deepak.saha@tothenew.com>
2019-03-06 17:12:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/linearlinear/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.linearlinear;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_restaurants_menu);
+        setContentView(R.layout.order_list);
+
+    }
+}
---
+++ app/src/main/java/com/example/linearlinear/RestaurantsMenu.java
@@ -0,0 +1,13 @@
+package com.example.linearlinear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantsMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants_menu);
+    }
+}
